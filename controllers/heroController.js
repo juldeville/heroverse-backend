18ce070ac73afc6fd6e4457f1c24d9ae709ec9b0
Julien <j.deville@protonmail.com>
2024-12-17 22:52:26 +0100

    modified route name

--- controllers/heroController.js
+++ controllers/heroController.js
@@ -1,6 +1,7 @@
 const heroverseUrl = "https://www.superheroapi.com/api";
 const apiKey = process.env.API_KEY;
-const fetchHeroes = async (req, res) => {
+
+const fetchHeroesBatch = async (req, res) => {
   const batch = +req.query.batch || 1;
   const batchSize = 8;
   const startId = (batch - 1) * batchSize + 1;
@@ -13,13 +14,29 @@ const fetchHeroes = async (req, res) => {
   try {
     const heroesData = await Promise.all(promises);
     if (heroesData) {
-      res.json({ result: true, heroes: heroesData });
+      res.status(200).json({ result: true, heroes: heroesData });
     } else {
-      res.json({ result: false, error: "No hero found" });
+      res.status(404).json({ result: false, error: "No hero found" });
     }
   } catch (err) {
     console.error("error is:", err.message);
+    res.json({ result: false, error: err.message });
   }
 };
 
-module.exports = { fetchHeroes };
+const fetchHeroById = async (req, res) => {
+  console.log("query is: ", req.query.id);
+  try {
+    const result = await fetch(`${heroverseUrl}/${apiKey}/${req.query.id}`);
+    const data = await result.json();
+
+    data.response === "success"
+      ? res.status(200).json({ result: true, hero: data })
+      : res.status(404).json({ result: false, error: data.error });
+  } catch (error) {
+    console.error(`error: ${error.message}`);
+    res.status(500).json({ result: false, error: error.message });
+  }
+};
+
+module.exports = { fetchHeroesBatch, fetchHeroById };
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 var express = require("express");
 var router = express.Router();
-const { fetchHeroes } = require("../controllers/heroController");
+const { fetchHeroesBatch, fetchHeroById } = require("../controllers/heroController");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 
-router.get("/fetchHeroes", fetchHeroes);
+router.get("/fetchHeroes", fetchHeroesBatch);
+router.get("/fetchHeroById", fetchHeroById);
 
 module.exports = router;
